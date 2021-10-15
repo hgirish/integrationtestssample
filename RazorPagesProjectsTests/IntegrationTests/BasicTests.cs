a71b2cc4795e85cf36627b9a3ef606bbc595116f
Girish <girish@post.com>
2021-10-15 12:32:38 -0700

    Initial test project

---
+++ RazorPagesProjectsTests/IntegrationTests/BasicTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using RazorPagesProject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RazorPagesProjectsTests.IntegrationTests
+{
+  public  class BasicTests 
+        : IClassFixture<WebApplicationFactory<RazorPagesProject.Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public BasicTests(WebApplicationFactory<RazorPagesProject.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        public async Task Get_EndpointsReturnsSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status code 200-209
+            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
